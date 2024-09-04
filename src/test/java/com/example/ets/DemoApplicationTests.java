24cba80e6c338dbfa0eaa9b16672b84673918173
unknown <hegdevarsha15@gmail.com>
2024-09-04 09:08:00 +0530

    classes attribute is changed

--- src/test/java/com/example/ets/DemoApplicationTests.java
+++ src/test/java/com/example/ets/DemoApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.ets;
 
+import com.jsp.ets.EduTrackingSystemApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {DemoApplicationTests.class})
+@SpringBootTest(classes = {EduTrackingSystemApplication.class})
 class DemoApplicationTests {
 
 	@Test
