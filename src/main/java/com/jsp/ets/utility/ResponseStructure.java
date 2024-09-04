23a213a750302e4a01ff10f14be99960761c7824
varshahegde15 <hegdevarsha15@gmail.com>
2024-09-04 07:56:25 +0530

    Revert "Sonar cloud implementation"

--- src/main/java/com/jsp/ets/utility/ResponseStructure.java
+++ src/main/java/com/jsp/ets/utility/ResponseStructure.java
@@ -12,7 +12,7 @@ public class ResponseStructure<T> {
 	private T data;
 
 	public static <T> ResponseStructure<T> create(int status, String message, T data) {
-		ResponseStructure<T> structure = new ResponseStructure<>();
+		ResponseStructure<T> structure = new ResponseStructure<T>();
 		structure.setData(data);
 		structure.setMessage(message);
 		structure.setStatus(status);
