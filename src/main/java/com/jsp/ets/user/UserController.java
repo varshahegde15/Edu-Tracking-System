4fe4a564a9387b9bc324e7e6fbf647fbc231f75f
unknown <hegdevarsha15@gmail.com>
2024-09-11 11:22:46 +0530

    otp verification is completed

--- src/main/java/com/jsp/ets/user/UserController.java
+++ src/main/java/com/jsp/ets/user/UserController.java
@@ -1,6 +1,12 @@
 package com.jsp.ets.user;
 
 import com.jsp.ets.user.request_dtos.OtpRequestDto;
+import com.jsp.ets.utility.ErrorStructure;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,11 +32,21 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
+@Tag(name = "User Controller", description = "APIs for managing User.")
 public class UserController {
 
 	private UserService userService;
 	private CustomResponseBuilder responseBuilder;
 
+
+
+	@Operation(description = "The API endpoint is used to register a new Admin user into the second level cache."
+			+ " The endpoint validates the registration details and assigns the ADMIN role to the user.",
+			responses = {
+					@ApiResponse(responseCode = "201", description = "Admin created"),
+					@ApiResponse(responseCode = "400", description = "Bad Request, invalid inputs", content = @Content(schema = @Schema(anyOf = ErrorStructure.class))),
+					@ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(anyOf = RuntimeException.class)))
+			})
 	@PostMapping("/admins/register")
 	public ResponseEntity<ResponseStructure<UserResponse>> registerAdmin(
 			@RequestBody @Valid RegistrationRequestDTO registrationRequestDTO) throws MessagingException {
@@ -38,6 +54,17 @@ public class UserController {
 		return responseBuilder.success(HttpStatus.ACCEPTED, "Accepted the details, verify your email by submitting the otp", response);
 	}
 
+
+
+
+
+	@Operation(description = "The API endpoint is used to register a new HR into the second level cache."
+			+ " The endpoint validates the registration details and assigns HR role to the user.",
+			responses = {
+					@ApiResponse(responseCode = "201", description = "HR created"),
+					@ApiResponse(responseCode = "400", description = "Bad Request, invalid inputs", content = @Content(schema = @Schema(anyOf = ErrorStructure.class))),
+					@ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(anyOf = RuntimeException.class)))
+			})
 	@PostMapping("/hrs/register")
 	public ResponseEntity<ResponseStructure<UserResponse>> registerHR(
 			@RequestBody @Valid RegistrationRequestDTO registrationRequestDTO) throws MessagingException {
@@ -45,6 +72,17 @@ public class UserController {
 		return responseBuilder.success(HttpStatus.ACCEPTED, "Accepted the details, verify your email by submitting the otp", response);
 	}
 
+
+
+
+
+	@Operation(description = "The API endpoint is used to register a new Trainer into the second level cache."
+			+ " The endpoint validates the registration details and assigns Trainer role to the user.",
+			responses = {
+					@ApiResponse(responseCode = "201", description = "Trainer created"),
+					@ApiResponse(responseCode = "400", description = "Bad Request, invalid inputs", content = @Content(schema = @Schema(anyOf = ErrorStructure.class))),
+					@ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(anyOf = RuntimeException.class)))
+			})
 	@PostMapping("/trainers/register")
 	public ResponseEntity<ResponseStructure<UserResponse>> registerTrainer(
 			@RequestBody @Valid RegistrationRequestDTO registrationRequestDTO) throws MessagingException {
@@ -52,6 +90,17 @@ public class UserController {
 		return responseBuilder.success(HttpStatus.ACCEPTED, "Accepted the details, verify your email by submitting the otp", response);
 	}
 
+
+
+
+
+	@Operation(description = "The API endpoint is used to register a new Student into the second level cache."
+			+ " The endpoint validates the registration details and assigns Student role to the user.",
+			responses = {
+					@ApiResponse(responseCode = "201", description = "HR created"),
+					@ApiResponse(responseCode = "400", description = "Bad Request, invalid inputs", content = @Content(schema = @Schema(anyOf = ErrorStructure.class))),
+					@ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(anyOf = RuntimeException.class)))
+			})
 	@PostMapping("/students/register")
 	public ResponseEntity<ResponseStructure<UserResponse>> registerStudent(
 			@RequestBody @Valid RegistrationRequestDTO registrationRequestDTO) throws MessagingException {
@@ -59,12 +108,25 @@ public class UserController {
 		return responseBuilder.success(HttpStatus.ACCEPTED, "Accepted the details, verify your email by submitting the otp", response);
 	}
 
+
+
+
+	@Operation(description = "The API endpoint is used to verify email and otp."
+			+ " And registers the user to the database after successful validation",
+			responses = {
+					@ApiResponse(responseCode = "200", description = "User registered Successfully"),
+					@ApiResponse(responseCode = "400", description = "Bad Request, invalid inputs", content = @Content(schema = @Schema(anyOf = ErrorStructure.class))),
+			})
 	@PostMapping("/users/verify")
 	public ResponseEntity<ResponseStructure<UserResponse>> verifyUser(
 			@RequestBody @Valid OtpRequestDto otpRequestDto) {
 		UserResponse response = userService.verifyUser(otpRequestDto);
 		return responseBuilder.success(HttpStatus.OK, "Successfully registered the user", response);
 	}
+
+
+
+
 
 	@PutMapping("/students/{studentId}")
 	public ResponseEntity<ResponseStructure<StudentResponseDTO>> updateStudent(
