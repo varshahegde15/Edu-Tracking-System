29f0374c0586acb9ecf0ba390b7847e60a78b9ec
unknown <hegdevarsha15@gmail.com>
2024-09-11 12:12:10 +0530

    Created UserDetailsImpl class and implemented it with UserDetailService interface and added unimplemented method

--- src/main/java/com/jsp/ets/security/UserDetailsImpl.java
+++ src/main/java/com/jsp/ets/security/UserDetailsImpl.java
@@ -1,15 +1,11 @@
 package com.jsp.ets.security;
 
-import com.jsp.ets.user.Privilege;
 import com.jsp.ets.user.User;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 @AllArgsConstructor
 public class UserDetailsImpl implements UserDetails {
