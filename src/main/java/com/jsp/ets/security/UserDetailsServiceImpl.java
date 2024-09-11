529ec4606b0230fcb54a296a3e145812bcbb9c49
unknown <hegdevarsha15@gmail.com>
2024-09-11 12:30:24 +0530

    Added the bean of UserRepository in UserDetailService and added @Service and @AllArgConstructor annotation over the class.and created findByEmail method in UserRepository to return Optional<User>

--- src/main/java/com/jsp/ets/security/UserDetailsServiceImpl.java
+++ src/main/java/com/jsp/ets/security/UserDetailsServiceImpl.java
@@ -1,12 +1,25 @@
 package com.jsp.ets.security;
 
+import com.jsp.ets.user.User;
+import com.jsp.ets.user.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return Optional.ofNullable(userRepository.findByEmail(username))
+                .map(UserDetailsImpl::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
     }
 }
