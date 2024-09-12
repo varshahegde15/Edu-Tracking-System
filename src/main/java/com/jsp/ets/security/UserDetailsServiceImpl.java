2fe71674ea42082e13329cc4d026e1ecbd560b4f
unknown <hegdevarsha15@gmail.com>
2024-09-12 11:56:42 +0530

    Completed the set up for authentication

--- src/main/java/com/jsp/ets/security/UserDetailsServiceImpl.java
+++ src/main/java/com/jsp/ets/security/UserDetailsServiceImpl.java
@@ -6,9 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 @AllArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
