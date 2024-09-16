2d34d10d6d2b1987992e1279c1df6ea7db5a1126
unknown <hegdevarsha15@gmail.com>
2024-09-16 12:15:01 +0530

    We have set the clams in jwt payload

--- src/main/java/com/jsp/ets/security/JwtService.java
+++ src/main/java/com/jsp/ets/security/JwtService.java
@@ -1,7 +1,15 @@
 package com.jsp.ets.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
 
 @Service
 public class JwtService {
@@ -10,5 +18,18 @@ public class JwtService {
     private String secret;
 
     @Value("${my_app.jwt.access_expiry}")
-    private int access_expiry;
+    private long access_expiry;
+
+    public String jwt(String email, String userId, String role){
+       return Jwts.builder()
+                .setClaims(Map.of("userId",userId,"email",email,"role",role))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+(access_expiry*60*1000)))
+                        .signWith(getKey())
+                .compact();
+    }
+
+    private Key getKey(){
+       return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
 }
